5aba05ccc65bb22cb128a7d842344066abdd0274
Piotr Jamroz <pm.jamroz@gmail.com>
2017-07-20 01:33:30 +0100

    Move versioning to a separate script

---
+++ scripts/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+git config --global push.default simple
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git config credential.helper "store --file=.git/credentials"
+echo "https://${GITHUB_API_KEY}:@github.com" > .git/credentials
+npm version $VERSION_INCREMENT
+git push && git push --tags
