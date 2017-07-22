8e9cce6d481dd0e269280d3e49e53ac693aecbf4
Piotr Jamroz <pm.jamroz@gmail.com>
2017-07-22 11:12:58 +0100

    Separate flow for hotfixes

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+# ./version.sh path|minor|major
+
 git checkout master
 git config --global push.default simple
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GITHUB_API_KEY}:@github.com" > .git/credentials
-npm version $VERSION_INCREMENT
+npm version $1 -m "%s [skip ci]"
 git checkout -
