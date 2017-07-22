d08e00347f6c5ba41949ba91a050950db33a919a
Piotr Jamroz <pm.jamroz@gmail.com>
2017-07-22 13:35:17 +0100

    Switch to the main branch before deployemnt

--- scripts/version.sh
+++ scripts/version.sh
@@ -2,11 +2,9 @@
 
 # ./version.sh path|minor|major
 
-git checkout master
 git config --global push.default simple
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GITHUB_API_KEY}:@github.com" > .git/credentials
 npm version $1 -m "Create new version: %s [skip ci]"
-git checkout -
