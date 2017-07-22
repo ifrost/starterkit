06dbe39eb8e199f65b41b545955594e5bbf26b42
Piotr Jamroz <pm.jamroz@gmail.com>
2017-07-22 13:57:15 +0100

    Change commit message

--- scripts/version.sh
+++ scripts/version.sh
@@ -7,4 +7,4 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GITHUB_API_KEY}:@github.com" > .git/credentials
-npm version $1 -m "Create new version: %s [skip ci]"
+npm version $1 -m "v%s"
