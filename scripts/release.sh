9d563cdd78171de5677dc69ce7c5caa7f05b2a5e
Piotr Jamroz <pm.jamroz@gmail.com>
2017-08-18 21:47:21 +0100

    Use commit tags to trigger versioning and publishing to gh-pages

---
+++ scripts/release.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Script performs tagging and releasing to gh-pages
+
+# setup git
+git config --global push.default simple
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git config credential.helper "store --file=.git/credentials"
+echo "https://${GITHUB_API_KEY}:@github.com" > .git/credentials
+
+message=`git log -1 --pretty=%B`
+
+if [[ $message =~ "[major]" ]]; then
+  version="major"
+fi
+
+if [[ $message =~ "[minor]" ]]; then
+  version="minor"
+fi
+
+if [[ $message =~ "[patch]" ]]; then
+  version="patch"
+fi
+
+if [[ $version ]]; then
+  npm version $version -m "v%s"
+fi
+
+if [[ $message =~ "[gh-pages]" ]]; then
+    mkdir tmp/flags
+    touch tmp/flags/gh-pages
+fi
