f4daff0f98c23e3b19e6d4ac43945e20afeb2158
Travis CI <travis@travis-ci.org>
2017-11-30 23:48:24 +0000

    v0.19.1

--- dist/starterkit.js
+++ dist/starterkit.js
@@ -7,7 +7,7 @@
 		exports["starterkit"] = factory();
 	else
 		root["starterkit"] = factory();
-})(this, function() {
+})(typeof self !== 'undefined' ? self : this, function() {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
